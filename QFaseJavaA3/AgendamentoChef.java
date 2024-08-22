a8ec0d413b5e97cb9ed57dfddc91c7e3660bb03f
PEDRO KICH DE MELO <pedro_k_melo@estudante.sesisenai.org.br>
2024-08-22 19:20:17 -0300

    ultima atualização do projeto por um tempo

---
+++ QFaseJavaA3/AgendamentoChef.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgendamentoChef {
+
+    private int idAgendamentoChef; //id do agendamento
+    private int dataAgendamentoChef; //data do agendamento
+    private boolean pagamentoRealizadoAgendamentoChef; //estado do pagamento
+    private int valorAgendamentoChef; //valor do agendamento
+    private String nomeChefeAgendamentoChef; //nome do chef
+    private String nomeClienteAgendamentoChef; //nome do cliente
+    
+    private ArrayList f;
+    
+
+}
---
+++ QFaseJavaA3/ChefPessoal.java
@@ -0,0 +1,7 @@
+public class ChefPessoal extends Usuario{
+
+    private int idChefPessoal; //id do chef
+    private String especialidadeChefPessoal; //especialidade do chef
+
+
+}
